e6b3309e0dba3939a875b904af764a529b7484c1
FreeDoM <jstrfaheem065@gmail.com>
2023-05-17 13:37:15 +0530

    Minimal app working,

--- player/src/App.js
+++ player/src/App.js
@@ -1,10 +1,33 @@
 import VideoPlayer from "react-video-player-extended";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(0.7);
+
+  const onPlay = () => {
+    setIsPlaying(true);
+  };
+
+  const onPause = () => {
+    setIsPlaying(false);
+  };
+
+  const onVolume = (value) => {
+    setVolume(value);
+  };
+
   return (
     <div>
-      <VideoPlayer url="https://media.w3.org/2010/05/bunny/trailer.mp4" />
+      <VideoPlayer
+        url="https://media.w3.org/2010/05/bunny/trailer.mp4"
+        isPlaying={isPlaying}
+        volume={volume}
+        onPlay={onPlay}
+        onPause={onPause}
+        onVolume={onVolume}
+      />
     </div>
   );
 }
