35383f7a9cb8295f1077b010fc235da369c98b0d
FreeDoM <jstrfaheem065@gmail.com>
2023-05-17 17:06:02 +0530

    Backup,

--- player/src/App.js
+++ player/src/App.js
@@ -1,10 +1,28 @@
 import VideoPlayer from "react-video-player-extended";
 import "./App.css";
 import { useState } from "react";
+import config from "./config";
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [selectedMarker, setSelectedMarker] = useState();
   const [volume, setVolume] = useState(0.7);
+  const [clips, setClips] = useState([]);
+  const [addClip, setAddClip] = useState(false);
+  const [markers, setMarkers] = useState([]);
+
+  const [addClipStartMarker, setAddClipStartMarker] = useState();
+  const [addClipEndMarker, setAddClipEndMarker] = useState();
+
+  const controls = [
+    "Play",
+    "Progress",
+    "Time",
+    "Volume",
+    "LastFrame",
+    "NextFrame",
+    "AddMarker",
+  ];
 
   const onPlay = () => {
     setIsPlaying(true);
@@ -18,17 +36,81 @@ function App() {
     setVolume(value);
   };
 
+  const onMarkerAdded = (marker) => {
+    if (!addClipStartMarker) {
+      setAddClipStartMarker(marker);
+    } else if (addClipStartMarker?.time > marker?.time) {
+      setAddClipStartMarker(marker);
+    } else {
+      setAddClipEndMarker(marker);
+    }
+    if (addClipStartMarker && addClipEndMarker) {
+      setMarkers([addClipStartMarker, addClipEndMarker]);
+    }
+  };
+
+  const onMarkerClick = (marker) => {
+    setSelectedMarker({
+      ...marker,
+      // To restart the timer on more than one click
+      time: marker?.time + Math.random() / 1000000,
+    });
+  };
+
+  const onToggleAddClip = () => {
+    setAddClip((preState) => !preState);
+  };
+
   return (
     <div>
       <VideoPlayer
-        url="https://media.w3.org/2010/05/bunny/trailer.mp4"
+        url={config.uri}
         isPlaying={isPlaying}
         volume={volume}
         onPlay={onPlay}
         onPause={onPause}
         onVolume={onVolume}
         onProgress={() => {}}
+        controls={controls}
+        markers={markers}
+        onMarkerAdded={onMarkerAdded}
+        selectedMarker={selectedMarker}
+        onMarkerClick={onMarkerClick}
+        timeStart={selectedMarker?.time}
       />
+      {addClip ? (
+        <button onClick={onToggleAddClip}>Cancel</button>
+      ) : (
+        <button onClick={onToggleAddClip}>Add Clip</button>
+      )}
+      {addClip && (
+        <>
+          <br />
+          <select name="type" id="type">
+            <option value="start">Start</option>
+            <option value="main">Main</option>
+            <option value="end">End</option>
+          </select>
+          <label>{addClipStartMarker?.time || "Start Time"}</label>
+          <label>{addClipEndMarker?.time || "End Time"}</label>
+          <button>Save</button>
+        </>
+      )}
+
+      {clips.map((marker) => {
+        return (
+          <button
+            onClick={() => {
+              onMarkerClick(marker);
+              setIsPlaying(true);
+            }}
+            key={marker.id}
+          >
+            {marker.id}
+          </button>
+        );
+      })}
+      <div>{JSON.stringify(clips)}</div>
     </div>
   );
 }
