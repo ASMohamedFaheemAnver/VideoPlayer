b3c1c88f166fe05fef745d20a79d775d1935f334
FreeDoM <jstrfaheem065@gmail.com>
2023-05-17 13:08:22 +0530

    Player added,

--- player/src/App.js
+++ player/src/App.js
@@ -1,7 +1,12 @@
+import VideoPlayer from "react-video-player-extended";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <VideoPlayer url="https://media.w3.org/2010/05/bunny/trailer.mp4" />
+    </div>
+  );
 }
 
 export default App;
